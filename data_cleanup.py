70ddc7e200d1f60c5ffc50badf5aa06ad26e6142
Maxwell Gove <maxwell.gove@nyu.edu>
2017-05-01 17:29:15 -0400

    week of month

--- data_cleanup.py
+++ data_cleanup.py
@@ -1,6 +1,8 @@
 # import the necessary packages
 import pandas as pd
 import pickle
+import datetime
+from dateutil import parser
 
 data = pd.read_json('train.json')
 
@@ -31,22 +33,23 @@ for feature in dummyized_features:
   data[feature] = 0
 
 for index, row in data.iterrows():
-  features = row['features']
-  for feature in features:
+  for feature in row['features']
     if feature in dummyized_features:
       data.loc[index, feature] = 1
 
+data.drop()
+
 
 # count the photos
+data['photo_count'] = [len(photo_list) for photo_list in data['photos']]
 
-photo_counts = [len(photo_list) for photo_list in data['photos']]
-data['photo_count'] = photo_counts
-
-# week of month (1-4)
+# week of month (1-5)
 
 weeks = []
-for date in data['created']:
+for create_date in data['created']:
+  weeks.append((parser.parse(create_date).day - 1) // 7 + 1)
 
+data['week'] = weeks
 
 # finally, save the dataframe as a pickle for future use
 
